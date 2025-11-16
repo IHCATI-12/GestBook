78e5e3a4413aa8b3df5cf4e2b9eaa2968c77e648
Ulisses Soares <164263005+IHCATI-12@users.noreply.github.com>
2025-11-15 21:22:44 -0300

    Atualização adição de arquivo .env
    Alterações em session para garantir a leitura correta da url do banco de dados, ultilizando arquivo .env para segurança de dados importantes.

--- app/db/session.py
+++ app/db/session.py
@@ -1,11 +1,15 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./gestbook.db"
+load_dotenv()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+if SQLALCHEMY_DATABASE_URL is None:   # Verificação para garantir que a variável de ambiente está definida
+    raise ValueError("DATABASE_URL não está definida no .env")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
